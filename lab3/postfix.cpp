2c995883b5f05407d1b5ce497661145dd5687ca4
Jacob Beneski <jacobbeneski@gmail.com>
2017-06-26 12:26:00 -0500

    Start Lab 3

---
+++ lab3/postfix.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include "LinkedStack.h"
+
+LinkedStack stack();
+
+
+void postfix() {
+
+
+
+
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
