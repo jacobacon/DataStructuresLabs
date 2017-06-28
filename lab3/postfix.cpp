49026861277ddac3c3acf1e1f054a2ab9d2a5708
Jacob Beneski <jacobbeneski@gmail.com>
2017-06-28 17:01:51 -0500

    Lab 3 mostly works

--- lab3/postfix.cpp
+++ lab3/postfix.cpp
@@ -2,17 +2,108 @@
 
 #include "LinkedStack.h"
 
-LinkedStack stack();
+using namespace std;
+
+//Check if char is a numeral
+bool isNumeral(char c);
+
+//Check if char is an operator
+bool isOperator(char c);
+
+//Evaluate the operation
+double eval(int v1, int v2, char op);
 
 
-void postfix() {
+double postfix() {
+
+    string parseString;
+
+    cout << "Enter postfix to calculate: ";
+
+    cin >> parseString;
 
 
+    LinkedStack<int> numeralStack;
+    LinkedStack<char> operatorStack;
+
+
+    for (int i = 0; i < parseString.length(); i++) {
+        if (isOperator(parseString[i])) {
+            //operatorStack.push(parseString[i]);
+
+            int numeralOne = numeralStack.peek();
+            numeralStack.pop();
+            int numeralTwo = numeralStack.peek();
+            numeralStack.pop();
+
+            numeralStack.push(eval(numeralOne, numeralTwo, parseString[i]));
+
+            cout << "Value is: " << numeralStack.peek();
+
+        //cout << "Value is: " << eval(numeralOne, numeralTwo, parseString[i]);
+
+
+
+
+        } else {
+            numeralStack.push(parseString[i] - '0');
+            cout << numeralStack.peek() << endl;
+        }
+
+
+    }
+
+
+    //return numeralStack.peek();
 
 
 }
 
+bool isNumeral(char c) {
+
+}
+
+bool isOperator(char c) {
+    if ((c == '+') || (c == '-') || (c == '*') || (c == '/')) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+double eval(int v1, int v2, char op){
+
+    double returnVal;
+
+    switch(op){
+
+        case '+': {
+            returnVal = v1 + v2;
+            break;
+        }
+        case '-':{
+            returnVal = v2 - v1;
+            cout << returnVal;
+            break;
+        }
+        case '*': {
+            returnVal = v2 * v2;
+            break;
+        }
+        case '/': {
+            returnVal = v2 / v1;
+            break;
+        }
+    }
+
+    return returnVal;
+
+}
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+
+    postfix();
     return 0;
 }
